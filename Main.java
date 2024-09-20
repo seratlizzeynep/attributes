4e4fddde72152bcff88e776723f69482205eb49d
seratlizzeynep <zeynepaybuke.seratli@tedu.edu.tr>
2024-09-20 21:30:51 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package attributes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Product urun = new Product();
+		urun.name= "Laptop";
+		urun.id = 1;
+		urun.description = "Asus Laptop";
+		urun.price = 50000;
+		urun.stockAmount = 43;
+		
+		ProductManager urunyonetici= new ProductManager();
+		urunyonetici.Add(urun);
+				
+	}
+
+}
